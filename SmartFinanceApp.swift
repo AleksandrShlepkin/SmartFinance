e455c092072c911129db319220eae40a1362da0a
White_Bishop <Shlepkinaleksandr@mail.ru>
2023-01-29 21:58:05 +0300

    Create app

---
+++ SmartFinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartFinanceApp.swift
+//  SmartFinance
+//
+//  Created by Mac on 29.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SmartFinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
