21ae3059361fafc58a4c71d9143c09072b172f8f
White_Bishop <Shlepkinaleksandr@mail.ru>
2023-02-23 22:19:57 +0300

    Add first view with List and Picker

---
+++ CardView/CardView.swift
@@ -0,0 +1,20 @@
+//
+//  CardView.swift
+//  SmartFinance
+//
+//  Created by Mac on 22.02.2023.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        Text("Card View")
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView()
+    }
+}
--- MainTabBar.swift
+++ MainTabBar.swift
@@ -11,39 +11,24 @@ struct MainTabBar: View {
     
     @State private var selectedButton = 0
     var body: some View {
-        VStack {
-            Spacer()
-            ZStack {
-                HStack {
-                    Button(action: {
-                        self.selectedButton = 0
-                    }) {
-                        Image("dollar")
-                            
-                    }.foregroundColor(self.selectedButton == 0 ? .black : .gray)
-                    Spacer(minLength: 12)
-                    
-                    Button(action: {
-                        self.selectedButton = 1
-                    }) {
-                        Image("statistics")
-                    }.foregroundColor(self.selectedButton == 1 ? .black : .gray)
-                }
-            }
-        }
-        //        TabView {
-        //            ExpensesView()
-        //                .tabItem {
-        //                    Text("First")
-        //                }
-        //            StatisticsView()
-        //                .tabItem {
-        //                    Text("Second")
-        //                }
-        //        }
-        //        .font(.headline)
-        //        .accentColor(.purple)
         
+                TabView {
+                    ExpensesView()
+                        .tabItem {
+                            Text("First")
+                            
+                        }
+                    StatisticsView()
+                        .tabItem {
+                            Text("Second")
+                        }
+                    CardView()
+                        .tabItem {
+                            Text("Third")
+                        }
+                }
+                .font(.headline)
+                .accentColor(.purple)
     }
 }
 
